0b3b272ed91c4cad990afeb7a0d4ed5b81577ba1
Jonas BELLESSA <jbelless@e3r11p7.42.fr>
2016-01-29 16:32:38 +0100

    wolf poper sur le net

--- srcs/ft_creat_env.c
+++ srcs/ft_creat_env.c
@@ -6,7 +6,7 @@
 /*   By: jbelless <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/27 10:44:56 by jbelless          #+#    #+#             */
-/*   Updated: 2016/01/29 09:48:14 by jbelless         ###   ########.fr       */
+/*   Updated: 2016/01/29 16:32:14 by jbelless         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,66 @@ void	ft_modim(t_env *e)
 		double raydiry = e->ydir + e->yscreen * camerax;
 		int mapx = (int)rayposx;
 		int mapy = (int)rayposy;
+		double sidedistx;
+		double sidedisty;
+		double deltadistx = sqrt(1 + (raydiry * raydiry) / (raydirx / raydirx));
+		double deltadisty = sqrt(1 + (raydirx * raydirx) / (raydiry / raydiry));
+		double perpwalldist;
+		int stepx;
+		int stepy;
+		int hit = 0;
+		int side;
+		if (raydirx < 0)
+		{
+			stepx = -1;
+			sidedistx = (rayposx - mapx) * deltadistx;
+		}
+		else
+		{
+			stepx = 1;
+			sidedistx = (1.0 - rayposx + mapx) * deltadistx;
+		}
+		if (raydiry < 0)
+		{
+			stepy = -1;
+			sidedisty = (rayposy - mapy) * deltadisty;
+		}
+		else
+		{
+			stepx = 1;
+			sidedisty = (1.0 - rayposy + mapy) * deltadisty;
+		}
+		while (hit == 0)
+		{
+			if (sidedistx < sidedisty)
+			{
+				sidedistx += deltadistx;
+				mapx += stepx;
+				side = 0;
+			}
+			else
+			{
+				sidedisty += deltadisty;
+				mapy += stepy;
+				side = 1;
+			}
+			if (e->map[mapx][mapy] > 0 )
+				hit = 1;
+		}
+		if (side == 0)
+			perpwalldist = fabs((mapx - rayposx + (1 - stepx) /2) / raydistx);
+		else
+			perpwalldist = fabs((mapy - rayposy + (1 - stepy) /2) / raydisty);
+		int lineheight = abs((int)(SIZE_W / perpwalldist));
+		int drawstart = -lineheight / 2 + SIZE_W / 2;
+		if (drawstart < 0)
+			drawstart = 0;
+		int drawend = lineheight / 2 + SIZE_W / 2;
+		if (drawend > SIZE_W)
+			drawend = SIZE_W - 1;
+			drawstart = 0;
+		ft_putline(x, drawstart, drawend, map[mapx][mapy]);
+		x++;
 	}
 }
 
